a2817a27c4412fbbe910c70c5ab22d770ddb0976
yincw <120758164@qq.com>
2024-10-21 16:08:26 +0800

    add

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -21,7 +21,7 @@ function HomepageHeader() {
         <div className={styles.buttons}>
           <Link
             className="button button--secondary button--lg"
-            to="/docs/intro">
+            to="/docs/guide/intro/">
             开始 - 5min ⏱️
           </Link>
         </div>
