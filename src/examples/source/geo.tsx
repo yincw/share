43c0515ba88e8e7a69653b587888e4f58c0bc0c8
yincw <120758164@qq.com>
2025-06-22 01:31:28 +0800

    add

--- src/examples/source/geo.tsx
+++ src/examples/source/geo.tsx
@@ -1,4 +1,4 @@
-import React, { JSX, useState, useCallback } from "react";
+import React, { JSX, useState, useCallback, useEffect } from "react";
 import { RMap, RLayerTile, RLayerVector, RStyle } from "rlayers";
 import { Feature } from "ol";
 import { fromLonLat } from "ol/proj";
@@ -6,7 +6,7 @@ import { Point } from "ol/geom";
 import GeoJSON from "ol/format/GeoJSON";
 import "ol/ol.css";
 
-import geojsonFeatures from "./wh.json";
+import geojsonFeatures from "./jx.json";
 
 // 武汉市-江夏区
 const origin = [114.322000, 30.375699];
@@ -17,17 +17,18 @@ const Geo = (): JSX.Element => {
 	const url = "http://webrd0{1-4}.is.autonavi.com/appmaptile?lang=zh_cn&size=1&scale=1&style=8&x={x}&y={y}&z={z}";
   const initial = {
     center: fromLonLat(origin),
-    zoom: 11
+    zoom: 10
   };
 
-  const data = new GeoJSON({
-    featureProjection: "EPSG:3857",
-    // featureClass: Feature
-  })
-  .readFeatures(geojsonFeatures) as Feature<Point>[];
+  const data = new GeoJSON()
+  .readFeatures(geojsonFeatures, {
+    featureProjection: 'EPSG:3857'
+  }) as Feature<Point>[];
 
-  console.log("data", data);
   
+  useEffect(() => {
+    // console.log(flow);
+  }, [flow]);
 
   return (
     <RMap
@@ -41,15 +42,19 @@ const Geo = (): JSX.Element => {
       />
       <RLayerVector
         zIndex={10}
-        features={
-          data
-        }
+        features={data}
+        onClick={useCallback(
+          (e) => {
+            // console.log("e", e.target.values_.name);
+            setFlow([...flow, e.target.values_.name].slice(-16));
+          },
+          [flow]
+        )}
       >
         <RStyle.RStyle>
-          <RStyle.RCircle radius={5}>
-            <RStyle.RFill color="blue" />
-          </RStyle.RCircle>
-        </RStyle.RStyle>
+            <RStyle.RStroke color="#007bff" width={2} />
+            <RStyle.RFill color="#ff000060" />
+          </RStyle.RStyle>
       </RLayerVector>
     </RMap>
   );
--- src/examples/source/osm.tsx
+++ src/examples/source/osm.tsx
@@ -1,11 +1,7 @@
-import React, { JSX, useState, useCallback } from "react";
-import { RMap, ROSM, RLayerVector, RStyle } from "rlayers";
-import { Feature } from "ol";
+import React, { JSX } from "react";
+import { RMap, ROSM } from "rlayers";
 import { fromLonLat } from "ol/proj";
-import { Point } from "ol/geom";
-import GeoJSON from "ol/format/GeoJSON";
 import "ol/ol.css";
-import geojsonFeatures from "./jx.json";
 
 // 武汉市-江夏区
 const origin = [114.322000, 30.375699];
@@ -16,12 +12,6 @@ const OSM = (): JSX.Element => {
     zoom: 11
   };
 
-  const data = new GeoJSON({
-    featureProjection: "EPSG:3857",
-    // featureClass: Feature
-  })
-  .readFeatures(geojsonFeatures) as Feature<Point>[];
-
   return (
     <RMap
       width={"100%"}
@@ -29,18 +19,6 @@ const OSM = (): JSX.Element => {
       initial={initial}
     >
       <ROSM />
-      <RLayerVector
-        zIndex={10}
-        features={
-          data
-        }
-      >
-        <RStyle.RStyle>
-          <RStyle.RCircle radius={5}>
-            <RStyle.RFill color="blue" />
-          </RStyle.RCircle>
-        </RStyle.RStyle>
-      </RLayerVector>
     </RMap>
   );
 }
